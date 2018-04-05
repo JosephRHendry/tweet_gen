77eb465eeda0d648d59c46859fcdb766e05700bf
Joseph Hendry <38083167+JosephRHendry@users.noreply.github.com>
2018-04-04 17:51:20 -0700

    Update tweet_gen_4.py

--- tweet_gen_4.py
+++ tweet_gen_4.py
@@ -1,5 +1,5 @@
-#Machine Learning componant. Reads from a text of tweets and learns to create in that style.
-
+""" Machine Learning componant. Reads from a text of tweets and learns to create in that style.
+Initially based on Keras LSTM example code """
 
 from __future__ import print_function
 from keras.callbacks import LambdaCallback
